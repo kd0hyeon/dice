5a0f0e3ad84940caa2d0fdde9a1fd1b47a4dc638
kd0hyeon <168632922+kd0hyeon@users.noreply.github.com>
2024-06-26 15:24:17 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,13 @@
+num1,num2,num3 = map(int, input().split())
+
+if num1 == num2 == num3:
+    print(10000+num1*1000)
+elif num1 == num2 :
+    print(1000+num1*100)
+elif num2 == num3:
+    print(1000+num2*100)
+elif num1 == num3:
+    print(1000+num3*100)
+else:
+    result = max(num1, num2, num3)
+    print(result*100)
